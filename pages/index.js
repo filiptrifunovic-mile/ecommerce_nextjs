503697390d4335ac8112b8f25b80443a1512a3f6
Filip <filip.trifunovic23@gmail.com>
2023-01-17 15:04:33 +0100

    herobanner layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product1", "Product2"].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {["Product1", "Product2"].map((product) => product)}
+      </div>
+      <FooterBanner />
     </>
   );
 };
