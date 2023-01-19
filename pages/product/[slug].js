4600da14a32591dd4fa0abbd10ca281d23a01453
Filip <filip.trifunovic23@gmail.com>
2023-01-19 16:47:43 +0100

    getStaticPaths

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -9,7 +9,7 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src="" alt="" />
+            <img src={urlFor(image && image[0])} alt="" />
           </div>
         </div>
       </div>
@@ -17,11 +17,32 @@ const ProductDetails = ({ product, products }) => {
   );
 };
 
+export const getStaticPaths = async () => {
+  const query = `*[_type == 'product'] {
+    slug {
+      current
+    }
+  }`;
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = '*[_type == "product"]';
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
+
+  console.log(product);
 
   return {
     props: { products, product },
